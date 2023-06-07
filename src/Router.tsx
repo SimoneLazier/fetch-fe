646fe0fcc2335f7bbddf80b5a1cdf10d1503986d
Simone Lazier <slazie2@uic.edu>
2023-06-07 13:47:13 -0500

    Fix error handling

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import Dog from './pages/Dog'
 import useAuth from './composables/useAuth'
 import AppLayout from './layouts/AppLayout'
 import Error500 from './pages/errors/500'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 function PrivateRoute({ children }: { children: JSX.Element }) {
   const { isLogged } = useAuth()
@@ -16,9 +16,9 @@ function PrivateRoute({ children }: { children: JSX.Element }) {
 function Router() {
   const { isLogged, logout } = useAuth()
 
-  axios.interceptors.response.use((res) => {
-    if (res.status === 401) logout()
-    return res
+  axios.interceptors.response.use(undefined, (error) => {
+    if (error instanceof AxiosError && error.response?.status === 401) logout()
+    return Promise.reject(error)
   })
 
   return (
